858b41aaf748e88561c76d8a4ba4818b327326e7
jeremy <zengxiankang9473@gmail.com>
2024-07-09 10:59:18 +0800

    add file

--- SocketServer.py
+++ SocketServer.py
@@ -55,7 +55,7 @@ class Server:
         self.host = "0.0.0.0"
         self.port = args.port
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10240000)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1048576)
         self.s.bind((self.host, self.port))
         self.tmp_recv_file = 'tmp/server_received.wav'
         self.tmp_proc_file = 'tmp/server_processed.wav'
